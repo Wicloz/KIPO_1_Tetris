21f3b44e6a7d80afc49b44c6dd3927837b4bf396
Unknown <hermes31@hotmail.nl>
2018-02-15 15:31:19 +0100

    choose playing type

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,33 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 3 && argc != 4) {
-        cout << "Usage: " << argv[0] << " <height> <width>" << endl;
-        cout << "Or:    " << argv[0] << " <height> <width> <seed>" << endl;
+    if (argc != 4 && argc != 5) {
+        cout << "Usage: " << argv[0] << " <playing type {1, 2, 3}> <height> <width>" << endl;
+        cout << "Or:    " << argv[0] << " <playing type {1, 2, 3}> <height> <width> <seed>" << endl;
         return 1;
     }//if
-    int h = atoi(argv[1]);
-    int w = atoi(argv[2]);
+    int h = atoi(argv[2]);
+    int w = atoi(argv[3]);
     Tetris board(h, w);
-    if (argc == 3)
+    if (argc == 4)
         srand(time(NULL));
     else
-        srand(atoi(argv[3]));
+        srand(atoi(argv[4]));
 
-    board.playSmarterGame(true);
+    switch (atoi(argv[1])) {
+        case 1:
+            board.playRandomGame(true);
+            break;
+        case 2:
+            board.playSmartGame(true);
+            break;
+        case 3:
+            board.playSmarterGame(true);
+            break;
+        default:
+            cerr << "Invalid playing type!" << endl;
+            return -1;
+    }
     board.statistics();
 
     return 0;
