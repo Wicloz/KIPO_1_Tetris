e15aec2d99a231d4d2749c1664ffef330dcb8801
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-21 16:55:30 +0100

    ruim nieuwe stuff op

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[]) {
 
     if (argc != 4 && argc != 5) {
-        cout << "Usage: " << argv[0] << " <playing type {1, 2, 3}> <height> <width>" << endl;
-        cout << "   Or: " << argv[0] << " <playing type {1, 2, 3}> <height> <width> <seed>" << endl;
+        cout << "Usage: " << argv[0] << " <playing type> <height> <width>" << endl;
+        cout << "   Or: " << argv[0] << " <playing type> <height> <width> <seed>" << endl;
         return 1;
     }
 
@@ -20,15 +20,14 @@ int main(int argc, char *argv[]) {
 
     if (string(argv[1]) == "Random" || string(argv[1]) == "random" || string(argv[1]) == "R" || string(argv[1]) == "r")
         board.playRandomGame(false);
-    else if (string(argv[1]) == "Smart" || string(argv[1]) == "smart" || string(argv[1]) == "S" ||
-             string(argv[1]) == "s")
+    else if (string(argv[1]) == "Smart" || string(argv[1]) == "smart" || string(argv[1]) == "S" || string(argv[1]) == "s")
         board.playSmartGame(false);
-    else if (string(argv[1]) == "Smarter" || string(argv[1]) == "smarter" || string(argv[1]) == "Ss" ||
-             string(argv[1]) == "ss")
+    else if (string(argv[1]) == "Smarter" || string(argv[1]) == "smarter" || string(argv[1]) == "Ss" || string(argv[1]) == "ss")
         board.playSmarterGame(false);
+
     else {
         cerr << "Invalid playing type!" << endl;
-        return -1;
+        return 1;
     }
 
     board.statistics();
