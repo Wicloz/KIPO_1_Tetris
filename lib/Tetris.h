76964bfae1ebe4e990f8761f49950f9894509cf8
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-21 16:52:25 +0100

    more cleanup

--- lib/Tetris.h
+++ lib/Tetris.h
@@ -22,45 +22,27 @@ private:
     bool board[hMAX][wMAX];     // the game board; board[i][j] true <=> occupied
     int piececount;             // number of pieces that has been used so far
     int rowscleared;            // number of rows cleared so far
+
 public:
     void clearFullRows();
-
     void displayBoard();
-
     void dropPiece(PieceName piece, int orientation, int position);
-
     void printInfoCurrentPiece(PieceName piece, int orientation, int position);
-
     bool endOfGame();
-
     Tetris(int height, int width);
-
     Tetris();
-
     void statistics();
-
     int possibilities(PieceName piece);
-
     void computeOrAndPos(PieceName piece, int &orientation, int &position, int themove);
-
     void topRow(bool *therow, int &numberrow, int &empties);
-
     int numberOfEmpties(int numberrow);
-
     int getSmartScore();
-
     void playGameSkeleton(bool output, void (*callback)(Tetris&, PieceName, int&, int&, double&));
-
     void playRandomGame(bool output);
-
     void playSmartGame(bool output);
-
     void playSmarterGame(bool output);
-
     static void randomGameDetermineBest(Tetris& tetris, PieceName randomPiece, int& bestOrientation, int& bestPosition, double& extra);
-
     static void smartGameDetermineBest(Tetris& tetris, PieceName randomPiece, int& bestOrientation, int& bestPosition, double& extra);
-
     static void smarterGameDetermineBest(Tetris& tetris, PieceName randomPiece, int& bestOrientation, int& bestPosition, double& extra);
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,4 +34,5 @@ int main(int argc, char *argv[]) {
     board.statistics();
 
     return 0;
+
 }//main
