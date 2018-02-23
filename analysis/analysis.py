0cc367ba91e4006e6e8dfb2c6cd52bcb3b4a3ff6
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-23 19:15:41 +0100

    Multithreaden voor analyse

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,38 +3,54 @@ from random import sample
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-
-# Start
-print('**This script performs a comparison of 4 different AI playing styles doing Tetris.**')
-print('**The results are the number of turns per game.**')
+from multiprocessing import Pool
 
 # Parameters
 boardSize = ['20', '10']
 playingTypes = ['Random', 'Smart', 'Smarter', 'Smartest']
-n = 40
-fileLocation = '../tetris'
+n = 30
+fileLocation = '../cmake-build-debug/KIPO_1_Tetris.exe'
 
-# n random seeds
-seeds = [str(x) for x in sample(range(100000, 100000000), n)]
+# Functions
+def runAnalysis(args):
+    output = sp.check_output([fileLocation, args[0], boardSize[0], boardSize[1], args[1]], universal_newlines=True)
+    print('#', end='', flush=True)
+    return {
+        'type': args[0],
+        'seed': args[1],
+        'result': int((output.splitlines()[3]).split(' ', 1)[0])
+    }
 
-# Results storage
-results = np.empty((n, len(playingTypes)))
+if __name__ == '__main__':
+    # Start
+    print('**This script performs a comparison of ' + str(len(playingTypes)) + ' different AIs playing Tetris.**')
+    print('**The results are the number of turns per game.**')
 
-# Play Tetris
-for type in playingTypes:
-    for seed in seeds:
-        output = sp.check_output([fileLocation, type, boardSize[0], boardSize[1], seed], universal_newlines=True)
-        results[seeds.index(seed)][playingTypes.index(type)] = int((output.splitlines()[3]).split(' ', 1)[0])
+    # n random seeds
+    seeds = [str(x) for x in sample(range(100000, 100000000), n)]
 
-# Calculate average
-result = [sum(x) for x in zip(*results)]
-result[:] = [x/len(seeds) for x in result]
+    # Results storage
+    results = np.empty((n, len(playingTypes)))
 
-# Display results
-print("For n = " + str(n))
-print()
-for x in playingTypes:
-    print(x + ': ' + str(result[playingTypes.index(x)]))
+    # Play Tetris
+    pool = Pool()
+    inputs = [[type, seed] for type in playingTypes for seed in seeds]
 
-pd.DataFrame(results, columns=playingTypes).plot.box()
-plt.pyplot.show()
+    print()
+    print('Analysing - ', end='', flush=True)
+    for output in pool.map(runAnalysis, inputs):
+        results[seeds.index(output['seed'])][playingTypes.index(output['type'])] = output['result']
+    print(' - Done!')
+
+    # Calculate average
+    result = [sum(x) for x in zip(*results)]
+    result[:] = [x/len(seeds) for x in result]
+
+    # Display results
+    print()
+    print("For n = " + str(n))
+    for x in playingTypes:
+        print(x + ': ' + str(result[playingTypes.index(x)]))
+
+    pd.DataFrame(results, columns=playingTypes).plot.box()
+    plt.pyplot.show()
